4be7ab9c36c44b90d4b5ab34af76ff2a07e7fef3
rgoldsberry <107135474+rgoldsberry@users.noreply.github.com>
2022-08-08 16:09:46 -0600

    added first three routes

--- app.py
+++ app.py
@@ -1,11 +1,70 @@
-from flask import Flask
+#dependencies
+from flask import Flask, jsonify
+import datetime as dt
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+#connect and reflect data from hawaii.sqlite
+engine = create_engine('sqlite:///hawaii.sqlite')
+Base = automap_base()
+Base.prepare(engine, reflect = True)
+
+# assign sql tables to classes
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1> Hello World </h1>'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API! <br>
+    Available Routes: <br>
+    /api/v1.0/precipitation <br>
+    /api/v1.0/stations <br>
+    /api/v1.0/tobs <br>
+    /api/v1.0/temp/start/end
+    ''')
 
-@app.route('/FAQ')
-def FAQ():
-    return "You shouldn't have any questions"
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    
+    precip = {date: prcp for date, prcp in precipitation}
+
+    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    results = session.query(Station.station).all()
+    
+    stations = list(np.ravel(results))
+    
+    return jsonify(stations=stations) 
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    session = Session(engine)
+
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.tobs).\
+        filter((Measurement.station == 'USC00519281') & (Measurement.date >= prev_year)).all()
+    
+    temps = list(np.ravel(results))
+
+    return jsonify(temps = temps)
